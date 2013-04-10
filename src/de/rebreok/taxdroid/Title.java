bc6f0e5fc8d242b34a25ef5e9772726138ae6e1d
Kai <kai@rebreok.de>
2013-04-10 17:35:00 +0200

    Freshly created project

---
+++ src/de/rebreok/taxdroid/Title.java
@@ -0,0 +1,15 @@
+package de.rebreok.taxdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Title extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
